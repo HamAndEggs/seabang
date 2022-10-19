08ce548c5051715f66a107e3ac01ac60ac4b757b
Richard <richard@hex-edge.com>
2022-10-19 08:50:48 +0100

    Cleaned up LFSR demo

--- examples/LFSR.cpp
+++ examples/LFSR.cpp
@@ -1,4 +1,4 @@
-#!/usr/local/bin/seabang --debug
+#!/usr/local/bin/seabang
 // Build as debug so the asserts will fire if I screw up.
 
 // The follow doc was used for the 32 bit taps for a 2n-1 repetition time.
@@ -6,13 +6,16 @@
 #include <iostream>
 #include <assert.h>
 #include <unistd.h>
+#include <cstring>
 
+uint32_t static LFSR_A = 0;
+uint32_t static LFSR_B = 0;
+
+static void UpdateLFSR(uint32_t &LFSR)
+{
 // Using 32,22,2,1
 // Rather anoyingly the document uses 1 based bit index. 
 // So really using 31, 21, 1, 0
-uint32_t static LFSR = 0;
-static uint32_t GetLFSR()
-{
     assert(LFSR != 0); // NO! Not allowed!
 
     // Make a new least significant bit from the taps.
@@ -24,32 +27,39 @@ static uint32_t GetLFSR()
     // Set the least significant but. Because of the shift above, LSB in the LFSR should be zero.
     // So if the new bit is zero, will stay that way. Means we do not have to clear the bit first.
     LFSR |= LSB;
-
-    return LFSR; // LFSR can never be zero, this function will never return zero.
 }
+
+// Uses two LFSR's cycling at a different rate so that you can not discover their state from sampling the results.
+static uint32_t GetLFSR()
+{
+    UpdateLFSR(LFSR_A);
+    UpdateLFSR(LFSR_B);
+    UpdateLFSR(LFSR_B);
+
+    return LFSR_A ^ LFSR_B;
+}
+
 
 int main(int argc, char *argv[])
 {
 // Say hello to the world!
-    std::cout << "Linear Feedback Shift Register example for a 32bit register\n";
-    std::cout << "The following sequence will take 4971 days to complete\n";
 
-    const uint32_t START_VALUE = 1;// You could set this to rand() but it will fail it rand() returns zero.
-    LFSR = START_VALUE;
-    uint64_t itterations = 0;
+    std::cout << "Linear Feedback Shift Register example for a 32bit register\n";
+    std::cout << "The following sequence will take 136 years to repeat\n";
+
+    LFSR_A = 0x00000001;
+    LFSR_B = 0xF00F000F;
+
     do
     {
-        assert( itterations < 0xffffffff );
-        GetLFSR();
+        const uint32_t LFSR = GetLFSR();
 
-        std::clog << LFSR%10; // As we're using modulo, we will get a zeros, which is good.
+        const uint32_t unit = LFSR%10; // As we're using modulo, we will get a zeros, which is good.
+        std::clog << unit;
+        sleep(1);
+    }while( true );
 
-        itterations++;
-        usleep(1000*100);
-    }while(LFSR != START_VALUE);
 
-    std::cout << "itterations == " << itterations << "\n";
-    assert( itterations == 0xffffffff );
     return EXIT_SUCCESS;
 }
 
